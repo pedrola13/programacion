d8cfc2ebcfbace62f73e5e3a2c0879c29fbf14f2
unknown <pedromorea@hotmail.com>
2021-06-09 17:19:56 -0300

    argrego un commit

---
+++ papeles/papeles.js
@@ -0,0 +1,60 @@
+
+    var opcionesA4 = [
+       "Obra 80Gr/m2", 
+"Ilustracion/Opalina 130Gr",
+"Ilustracion/Opalina 250Gr/m2",
+"Filmina",
+"Vegetal",
+"Adhesivo Ilustracion",
+"Adhesivo Obra",
+"Bookcel 80Gr/m2",
+"Fabriano Color 80Gr/m2",
+"Fabriano Color 160Gr/m2",
+"Fotografico Brillante",
+"Kraft 220Gr/m2",
+"Cartulina Encapada 250Gr/m2",
+"Stardream 240Gr/m2"
+    ]  
+
+    
+        var opcionesA3 = [
+           "Obra 80Gr/m2", 
+    "Ilustracion/Opalina 130Gr",
+    "Ilustracion/Opalina 250Gr/m2",
+    "Filmina",
+    "Vegetal",
+    "Fabriano Color 80Gr/m2",
+    "Fabriano Color 160Gr/m2",
+    "Kraft 220Gr/m2",
+    "Stardream 240Gr/m2"
+        ]  
+
+        
+            var opcionesSRA3 = [
+               "Obra 80Gr/m2", 
+        "Ilustracion/Opalina 130Gr",
+        "Ilustracion/Opalina 250Gr/m2",
+        "Ilustracion 300Gr/m2",
+        "Adhesivo Ilustracion",
+        "Adhesivo Obra",
+        "Cartulina Encapada 250Gr/m2"
+       
+            ]  
+
+
+            
+                var opcionesPloteo = [
+                   "Obra 90Gr/m2 ancho 60cm",
+                   "Obra 90Gr/m2 ancho 90cm", 
+            
+           
+                ]  
+    
+               
+                    var opcionesA2 = [
+                       "Opalina 250 Gr 420 x 594 mm",
+                        
+                    ]  
+
+            
+                
